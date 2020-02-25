b3b1fc1814efd1c347d4a145033157d25a7fd4d1
TREY <hugh.a.lloyd@gmail.com>
2020-02-24 23:04:04 -0600

    tested episodes

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -2,5 +2,9 @@ import axios from "axios"
 
 export const fetchShow = () => {
     return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
-    .then(res => {return res.data})
+    .then(res => {return res})
+    .catch(err => {
+        console.error('error fetching data',err.message);
+        return err;
+    })
 }
